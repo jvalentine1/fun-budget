e863dac77d40299b4afad83a42b81af3ec925065
Jake <jake@Marissas-MacBook-Pro-2.local>
2019-05-19 21:29:33 -0600

    initial html & css

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCpCy4zKSIEnJ4iKxM3kNuG5Vqw1azUSDQ",
+    authDomain: "fun-budget-app-jakev.firebaseapp.com",
+    databaseURL: "https://fun-budget-app-jakev.firebaseio.com",
+    projectId: "fun-budget-app-jakev",
+    storageBucket: "fun-budget-app-jakev.appspot.com",
+    messagingSenderId: "1064589997532",
+    appId: "1:1064589997532:web:719ecc24907661a8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
+  var database = firebase.database();
